6a12b0a39bd007030c6f8be15d1ab4997bd5242f
Summer T <summer@summertime-development.com>
2023-05-04 16:48:56 -0500

    complete

--- src/index.ts
+++ src/index.ts
@@ -5,55 +5,55 @@ import {
     validateFxn,
 } from './helpers.js';
 
-function subarraySum2(nums: number[], target: number): number {
-    const map = new Map();
-    map.set(0, 1);
-
+// dominating XOR - failed on long arrays
+function subarraySum(nums: number[]): number {
     let counter = 0;
-    let sum = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
-
-        if (map.has(sum - target)) {
-            counter += map.get(sum - target);
-        }
-
-        let newCounter = (map.has(sum) ? map.get(sum) : 0) + 1;
-        map.set(sum, newCounter);
-    }
-    return counter;
-}
-
-function subarraySum(nums: number[], target: number): number {
-    let counter = 0;
+    let map = new Set();
 
     for (let i = 0; i < nums.length; i++) {
         let firstNumber = nums[i];
-        let total = firstNumber;
+
         for (let j = i + 1; j < nums.length; j++) {
             let secondNumber = nums[j];
-            total += secondNumber;
 
-            if (total === target) {
+            let xor = firstNumber ^ secondNumber;
+            let and = firstNumber & secondNumber;
+            if (xor > and) {
                 counter++;
             }
-        }
-
-        if (firstNumber === target) {
-            counter++;
         }
     }
 
     return counter;
+}
+
+//api call to an api - passed on all expect 1 case
+const axios = require('axios');
+
+async function getDiscountedPrice(barcode) {
+    if (!barcode) {
+        return -1;
+    }
+
+    let response = await axios.get(
+        'https://jsonmock.hackerrank.com/api/inventory?barcode=' + barcode
+    );
+
+    console.log(response.data);
+
+    if (!response.data.data[0] || !response.data || !response.data.data[0]) {
+        return -1;
+    }
+
+    let { price, discount } = response.data.data[0];
+    let discountedPrice = price - (discount / 100) * price;
+
+    return Math.floor(discountedPrice);
 }
 
 consoleStart();
 
-validateFxn(subarraySum2([1, 1, 1], 2), 2);
-
-validateFxn(subarraySum([1, 2, 3], 3), 2);
-validateFxn(subarraySum([1, 1, 1], 2), 2);
+validateFxn(subarraySum([4, 3, 5, 2]), 4);
 
 consoleEnd();
 
